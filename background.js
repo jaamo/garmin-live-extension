17a89516ea05e09e7d87fe138f41a0d4b6ea917a
Jaakko Alajoki <jaamo@Jaakkos-MacBook-Pro.local>
2018-02-10 12:31:31 +0200

    Added correct date.

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 function openTotalsPage(tab) {
-	chrome.tabs.create({ url: "totals.html?date=2018-2-10" });
+	chrome.tabs.create({ url: "totals.html?date=2018-2-15" });
 }
   
 chrome.browserAction.onClicked.addListener(openTotalsPage);
